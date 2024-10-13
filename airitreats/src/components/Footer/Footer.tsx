9a03c50323b9903b83c24d58e1107f95c47e56e9
Michael <michaeldam.dev@gmail.com>
2024-10-13 16:58:33 -0600

    Added Google Map

--- airitreats/src/components/Footer/Footer.tsx
+++ airitreats/src/components/Footer/Footer.tsx
@@ -1,11 +1,22 @@
 import Newsletter from "@/components/Newsletter/Newsletter";
+import { createClient } from "@/prismicio";
+import styles from "./Footer.module.scss"
 import { toast } from "sonner";
 
 
-export default function Footer() {
+export default async function Footer() {
+    const client = createClient()
+    const details = await client.getSingle("footer")
+    console.log(details)
     return (
         <div>
-            Footer
+            <div className={`${styles.mapContainer}`}>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2369.3453439170416!2d-113.50105682277649!3d53.56945297235495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53a02395e6eebc97%3A0xbbc74ad142909e90!2sNAIT%20L%20building!5e0!3m2!1sen!2sca!4v1728859425183!5m2!1sen!2sca"
+                    width="100%"
+                    height="100%"
+                ></iframe>
+            </div>
             <Newsletter />
         </div>
     )
