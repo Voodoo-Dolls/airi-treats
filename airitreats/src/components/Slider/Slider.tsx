c877def770e6dd78fddd4751396d19358619af47
Michael <michaeldam.dev@gmail.com>
2024-10-14 18:26:28 -0600

    Added Keenslider

--- airitreats/src/components/Slider/Slider.tsx
+++ airitreats/src/components/Slider/Slider.tsx
@@ -1,16 +1,59 @@
 'use client'
 
+import { useKeenSlider } from "keen-slider/react"
+import 'keen-slider/keen-slider.min.css'
+
 import ProductCard from "../Products/ProductCard/ProductCard"
 
+
 export default function Slider({ list }) {
+    const [sliderRef] = useKeenSlider(
+        {
+            loop: true,
+        },
+        [
+            (slider) => {
+                let timeout
+                let mouseOver = false
+                function clearNextTimeout() {
+                    clearTimeout(timeout)
+                }
+                function nextTimeout() {
+                    clearTimeout(timeout)
+                    if (mouseOver) return
+                    timeout = setTimeout(() => {
+                        slider.next()
+                    }, 2000)
+                }
+                slider.on("created", () => {
+                    slider.container.addEventListener("mouseover", () => {
+                        mouseOver = true
+                        clearNextTimeout()
+                    })
+                    slider.container.addEventListener("mouseout", () => {
+                        mouseOver = false
+                        nextTimeout()
+                    })
+                    nextTimeout()
+                })
+                slider.on("dragStarted", clearNextTimeout)
+                slider.on("animationEnded", nextTimeout)
+                slider.on("updated", nextTimeout)
+            },
+        ]
+    )
     return (
         <>
-            {list.map((uid, index) => {
-                return (
-                    <ProductCard uid={uid} key={index} />
-                )
-            }
-            )}
+            <div ref={sliderRef} className="keen-slider">
+                {list.map((uid, index) => {
+                    return (
+                        <div className="keen-slider__slide" key={index} >
+                            <ProductCard uid={uid} />
+                        </div>
+                    )
+                }
+                )}
+            </div >
         </>
     )
 }
