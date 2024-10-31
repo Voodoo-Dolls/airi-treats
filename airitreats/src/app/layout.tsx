00bd4a14ffd6fb0fb1ede76c333dcd9c62daac7b
Michael <michaeldam.dev@gmail.com>
2024-10-31 12:39:57 -0600

    updated Metadata

--- airitreats/src/app/layout.tsx
+++ airitreats/src/app/layout.tsx
@@ -1,14 +1,13 @@
+import type { Metadata, ResolvingMetadata } from 'next'
 import { PrismicPreview } from "@prismicio/next";
-import { repositoryName } from "@/prismicio";
+import { createClient, repositoryName } from "@/prismicio";
 import Head from "next/head";
-
 import Header from "@/components/Header/Header";
 import "./snipcart.css"
 import "./global.scss"
 import { bodyFont, headerFont, subheadingFont, buttonFont } from "./fonts";
 import Footer from "@/components/Footer/Footer";
 import { Toaster } from "sonner";
-import Spinner from "@/components/Spinner/Spinner";
 import { Suspense } from "react";
 import Loading from "./loading";
 
@@ -17,6 +16,19 @@ import Loading from "./loading";
 
 
 
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient()
+
+  const page = await client.getSingle("settings")
+
+  return {
+    title: page.data.site_title || "Airitreats",
+    // description: page.data.me,
+    openGraph: {
+      images: [page.data.og_image.url || ""],
+    },
+  }
+}
 
 
 export default function RootLayout({
