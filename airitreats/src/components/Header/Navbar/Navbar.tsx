6e94ab114a3ed4d629152b1783d338aab72a91c1
Michael <michaeldam.dev@gmail.com>
2024-11-05 12:18:13 -0700

    Added Desktop Nav

--- airitreats/src/components/Header/Navbar/Navbar.tsx
+++ airitreats/src/components/Header/Navbar/Navbar.tsx
@@ -1,37 +1,45 @@
-
+'use client'
 import { createClient } from "@/prismicio";
 import { PrismicNextLink } from "@prismicio/next";
 import styles from "./Navbar.module.scss"
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 
 
 
 
 
-export default async function Navbar() {
-    const client = createClient();
-    const settings = await client.getSingle('settings')
+export default function Navbar({ links, logo }: any) {
+    const [isClient, setIsClient] = useState(false)
+    useEffect(() => {
+        setIsClient(true)
+    }, [])
+
 
 
     return (
-        <nav>
-            <ul className={styles.ul}>
-                {/* Get Nav Links in Prismic from Settings */}
-                {settings.data.links.map((data) => {
-                    return (
-                        <li key={data.label}><PrismicNextLink field={data.link}>{data.label}</PrismicNextLink></li>
-                    )
-                }
-                )}
-                {/* Snipcart Cart */}
-                <li>
-                    <a href="#" className="snipcart-user-profile">
-                        User profile
-                    </a>
-                </li>
+        <div className={`${styles.container} container`}>
+            <div className={styles.imageContainer}>
+                <Link href={"/"}>
+                    <Image src={logo} alt={`Airitreats Logo`} fill priority sizes="80px" />
+                </Link>
+            </div>
+            <nav>
+                <ul className={styles.ul}>
+                    {/* Get Nav Links in Prismic from Settings */}
+                    {links.map((data: any) => {
+                        return (
+                            <li key={data.label}><PrismicNextLink field={data.link}>{data.label}</PrismicNextLink></li>
+                        )
+                    }
+                    )}
+                    {/* Snipcart Cart */}
 
-                <li><a href="#" className="snipcart-checkout">Cart:<span className="snipcart-items-count"> </span></a></li>
-            </ul>
-        </nav>
+                    <li><a href="#" className="snipcart-checkout">Cart:&nbsp;{isClient ? <span className="snipcart-items-count"> </span> : ''}</a></li>
+                </ul>
+            </nav>
+        </div>
     )
 }
