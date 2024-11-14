f30e099b17656eb05ca7936beeba7d9df4b0ceac
Michael <michaeldam.dev@gmail.com>
2024-11-14 13:59:25 -0700

    Added Sold Out Functionality

--- airitreats/src/components/Products/ProductCard/ProductCard.tsx
+++ airitreats/src/components/Products/ProductCard/ProductCard.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image"
 import { createClient } from "@/prismicio";
 import Link from "next/link"
-import { headerFont } from "@/app/fonts"
 import styles from "./ProductCard.module.scss"
 import { useEffect, useState } from "react";
 import ProductSpinner from "@/components/Spinner/ProductSpinner/ProductSpinner";
@@ -36,9 +35,11 @@ export default function ProductCard({ uid }: any) {
             }
 
             <div className={styles.imgContainer}>
-                <Image src={data.main_image.url} fill alt="" sizes="300px" priority={false} />
+                <Image src={data.main_image.url} fill alt={`Picture of ${data.product_name}`} sizes="300px" priority={false} />
+                {!data.product_available && <div className={styles.sold}>SOLD OUT</div>}
             </div>
             <h3 className={styles.title}>{data.product_name}</h3>
+            {/* Checks if product is available then renders proper p tag */}
             <p className={styles.price}>${data.price.toFixed(2)}</p>
 
 
