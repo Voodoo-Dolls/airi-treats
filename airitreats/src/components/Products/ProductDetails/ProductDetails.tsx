d7288148b46ebde053ced3549d90658cec42c3c5
Michael <michaeldam.dev@gmail.com>
2024-11-14 09:22:36 -0700

    Updated Product Details

--- airitreats/src/components/Products/ProductDetails/ProductDetails.tsx
+++ airitreats/src/components/Products/ProductDetails/ProductDetails.tsx
@@ -30,32 +30,35 @@ export default function ProductDetails({ productData }: any) {
         <div className={`${styles.container}`}>
             <div className={styles.imageContainer}>
                 <PrismicNextImage field={main_image} className={styles.mainImage} alt="" priority />
-                <h2>{product_name}</h2>
+                <h2 className={styles.productName}>{product_name}</h2>
             </div>
-            <h3 className={styles.price}>${price.toFixed(2)}</h3>
-            <div className={`container`}>
-                <PrismicRichText field={description} />
-            </div>
-            <div className={`${styles.quantity} container`}>
-                <h3>Quantity</h3>
-                <div className={styles.controls}>
-                    <button onClick={() => { handleQuantity(-1) }}>-</button>
-                    <p>{quantity}</p>
-                    <button onClick={() => { handleQuantity(1) }}>+</button>
+            <div className={styles.content}>
+                <h2 className={styles.productName}>{product_name}</h2>
+                <h3 className={styles.price}>${price.toFixed(2)}</h3>
+                <div className={`container`}>
+                    <PrismicRichText field={description} />
                 </div>
-                <button
-                    className={`snipcart-add-item yellowBtn ${styles.yellowBtn}`}
-                    data-item-id={product_name}
-                    data-item-name={product_name}
-                    data-item-price={price}
-                    data-item-image={main_image.url}
-                    data-item-url={productData.url}
-                    data-item-description={description[0].text}
-                    data-item-quantity={quantity}
-                    onClick={() => toast.success(`${product_name} x${quantity} Added`)}
-                >
-                    Add to Cart
-                </button>
+                <div className={`${styles.quantity} container`}>
+                    <h3>Quantity</h3>
+                    <div className={styles.controls}>
+                        <button onClick={() => { handleQuantity(-1) }}>-</button>
+                        <p>{quantity}</p>
+                        <button onClick={() => { handleQuantity(1) }}>+</button>
+                    </div>
+                    <button
+                        className={`snipcart-add-item yellowBtn ${styles.yellowBtn}`}
+                        data-item-id={product_name}
+                        data-item-name={product_name}
+                        data-item-price={price}
+                        data-item-image={main_image.url}
+                        data-item-url={productData.url}
+                        data-item-description={description[0].text}
+                        data-item-quantity={quantity}
+                        onClick={() => toast.success(`${product_name} x${quantity} Added`)}
+                    >
+                        Add to Cart
+                    </button>
+                </div>
             </div>
 
         </div>
