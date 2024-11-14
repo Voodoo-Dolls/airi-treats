ef2a87be7b666bda363332b1f19f02590d38a29b
Michael <michaeldam.dev@gmail.com>
2024-11-14 15:05:42 -0700

    Added Controls to slider

--- airitreats/src/components/Slider/Slider.tsx
+++ airitreats/src/components/Slider/Slider.tsx
@@ -2,12 +2,22 @@
 import { useKeenSlider } from "keen-slider/react"
 import 'keen-slider/keen-slider.min.css'
 import ProductCard from "../Products/ProductCard/ProductCard"
+import { useState } from "react"
 
 export default function Slider({ list }: any) {
-    const [sliderRef] = useKeenSlider(
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const [loaded, setLoaded] = useState(false)
+    const [sliderRef, instanceRef] = useKeenSlider(
         {
+            initial: 0,
+            slideChanged(slider) {
+                setCurrentSlide(slider.track.details.rel)
+            },
             loop: true,
             mode: "free",
+            created() {
+                setLoaded(true)
+            },
             slides: {
                 perView: 2,
                 spacing: 16
@@ -43,7 +53,7 @@ export default function Slider({ list }: any) {
                     if (mouseOver) return
                     timeout = setTimeout(() => {
                         slider.next()
-                    }, 2000)
+                    }, 4000)
                 }
                 slider.on("created", () => {
                     slider.container.addEventListener("mouseover", () => {
@@ -64,7 +74,41 @@ export default function Slider({ list }: any) {
     )
     return (
         <>
-            <div ref={sliderRef} className="keen-slider">
+            <div className="navigation-wrapper">
+                <div ref={sliderRef} className="keen-slider">
+                    {list.map((uid: any, index: number) => {
+                        return (
+                            <div className="keen-slider__slide" key={index} >
+                                <ProductCard uid={uid} />
+                            </div>
+                        )
+                    }
+                    )}
+                </div>
+                {loaded && instanceRef.current && (
+                    <>
+                        <Arrow
+                            left
+                            onClick={(e) =>
+                                e.stopPropagation() || instanceRef.current?.prev()
+                            }
+                            disabled={currentSlide === 0}
+                        />
+
+                        <Arrow
+                            onClick={(e) =>
+                                e.stopPropagation() || instanceRef.current?.next()
+                            }
+                            disabled={
+                                currentSlide ===
+                                instanceRef.current.track.details.slides.length - 1
+                            }
+                        />
+                    </>
+                )}
+            </div>
+
+            {/* <div ref={sliderRef} className="keen-slider">
                 {list.map((uid: any, index: number) => {
                     return (
                         <div className="keen-slider__slide" key={index} >
@@ -73,7 +117,27 @@ export default function Slider({ list }: any) {
                     )
                 }
                 )}
-            </div >
+            </div > */}
         </>
+    )
+}
+
+function Arrow(props: any) {
+    const disabled = props.disabled ? " arrow--disabled" : ""
+    return (
+        <svg
+            onClick={props.onClick}
+            className={`arrow ${props.left ? "arrow--left" : "arrow--right"
+                } ${disabled}`}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+        >
+            {props.left && (
+                <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+            )}
+            {!props.left && (
+                <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+            )}
+        </svg>
     )
 }
