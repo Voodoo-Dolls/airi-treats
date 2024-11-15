e6dc6dcce5151651690e33ca9b18adc08f82284d
KenCap <145059469+KenCap@users.noreply.github.com>
2024-11-15 13:33:56 -0700

    footer styling
    touched up footer styling for mobile

--- airitreats/src/components/Footer/Footer.tsx
+++ airitreats/src/components/Footer/Footer.tsx
@@ -1,8 +1,7 @@
 import Newsletter from "@/components/Newsletter/Newsletter";
 import Map from "./Map/Map";
 import styles from "./Footer.module.scss"
-import { FaInstagram } from "react-icons/fa";
-import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram, FaFacebookF, FaPhone, FaMapMarkerAlt } from "react-icons/fa";
 
 
 export default function Footer() {
@@ -10,21 +9,31 @@ export default function Footer() {
     return (
         <footer className={styles.footer}>
             {/* <Map /> */}
-            <div className={styles.placeholder}>
-                <p> Map placeholder here</p>
-                <p> and contact details here</p>
+            <div className={styles.address}>
+                <h3> Come Find Us!</h3>
+                <div>
+                    <FaMapMarkerAlt title="our store"/>
+                    <p>13 Amherst Crescent, St. Albert, AB T8N 2P7</p>
+                </div>
+                
+                <div>
+                    <FaPhone title="phone number"/>
+                    <p>(XXX) XXX-XXXX</p>
+                </div>
             </div>
-            <div className={styles.socialMedia}>
-                <h3>Follow Us</h3>
-                <p><a href="https://www.instagram.com/airitreats/" target="_blank"><FaInstagram /></a></p>
-                <p><a href="https://www.facebook.com/profile.php?id=61553694755284" target="_blank"><FaFacebookF /></a></p>
 
-            </div>
+            <div className={styles.community}>
+                <div className={styles.socialMedia}>
+                    <h3>Follow Us</h3>
+                    <p><a href="https://www.instagram.com/airitreats/" target="_blank"><FaInstagram title="our instagram"/></a></p>
+                    <p><a href="https://www.facebook.com/profile.php?id=61553694755284" target="_blank"><FaFacebookF title="our facebook"/></a></p>
 
-            <div className={styles.subscription}>
-                <h3>Newsletter</h3>
-                <p>Do you want to know the latest deals, new arrivals, limited edition treats? Subscribe to our newsletter!</p>
-                <Newsletter />
+                </div>
+                <div className={styles.subscription}>
+                    <h3>Newsletter</h3>
+                    <p>Do you want to know the latest deals, new arrivals, limited edition treats? Subscribe to our newsletter!</p>
+                    <Newsletter />
+                </div>
             </div>
 
             <div className={styles.legalese}>
