1af482202786952b2c6b1403de443bd201ac95fe
M-Ragini <112775673+M-Ragini@users.noreply.github.com>
2024-11-16 10:20:20 -0700

    added tablet style for about us page except button link

--- airitreats/src/slices/SectionTitle/index.tsx
+++ airitreats/src/slices/SectionTitle/index.tsx
@@ -22,31 +22,37 @@ const SectionTitle = ({ slice }: SectionTitleProps): JSX.Element => {
           {/* .introWrapper contains title, description 1 and description 2 */}
           <h2 className={styles.title}>{slice.primary.title}</h2>
           <div className={styles.descriptionContainer}>
-            <p className={styles.description}><PrismicRichText field={slice.primary.description} /></p>
-            <p className={styles.descriptionOne}><PrismicRichText field={slice.primary.description_one} /></p>
-            <p className={styles.descriptionTwo}><PrismicRichText field={slice.primary.description_two} /></p>
+            <PrismicRichText field={slice.primary.description} />
+            <PrismicRichText field={slice.primary.description_one} />
+            <PrismicRichText field={slice.primary.description_two} />
           </div>
         </div>
-        <div className={styles.paragraphList}>
-          <p>{slice.primary.list_intro}</p>
-          {slice.primary.list.map((item, index) => (
-            <li key={index}>{item.bulletlist}</li>
-          ))}
+        <div className={styles.pListContainer}>
+          <div className={styles.paragraphList}>
+            <p>{slice.primary.list_intro}</p>
+            {slice.primary.list.map((item, index) => (
+              <li key={index}>{item.bulletlist}</li>
+            ))}
+          </div>
         </div>
-        <div className={styles.freezeDry}>
-          <h3><PrismicRichText field={slice.primary.paragraph_title} /></h3>
+        <div className={styles.freezeDryContainer}>
+          <div className={styles.freezeDryIntro}>
+            <h3 className={styles.freezeDry}><PrismicRichText field={slice.primary.paragraph_title} /></h3>
+            <div className={styles.freezeDryDesc}>
+              <p>{slice.primary.paragraph}</p>
+            </div>
+          </div>
+          <div className={styles.imgContainer}>
+            <p><PrismicNextImage field={slice.primary.image} /></p>
+          </div>
         </div>
-        <div className={styles.freezeDryDesc}>
-          <p>{slice.primary.paragraph}</p>
-        </div>
-        <div className={styles.imgContainer}>
-          <p><PrismicNextImage field={slice.primary.image} /></p>
-        </div>
-        <div className={styles.paragraph2}>
-          <p>{slice.primary.paragraph2}</p>
-        </div>
-        <div className={styles.paragraph3}>
-          <PrismicRichText field={slice.primary.paragraph3} />
+        <div className={styles.twoParaContainer}>
+          <div className={styles.paragraph2}>
+            <p>{slice.primary.paragraph2}</p>
+          </div>
+          <div className={styles.paragraph3}>
+            <PrismicRichText field={slice.primary.paragraph3} />
+          </div>
         </div>
         <div className={styles.closingWrapper}>
           <div className={styles.closingParagraph}><PrismicRichText field={slice.primary.closing_paragraph} /></div>
