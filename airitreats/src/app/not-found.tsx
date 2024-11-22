0f9ecfbe4221d3205af3d462f9b8bd85edca5fbb
KenCap <145059469+KenCap@users.noreply.github.com>
2024-11-22 15:11:23 -0700

    styled 404 page

--- airitreats/src/app/not-found.tsx
+++ airitreats/src/app/not-found.tsx
@@ -1,14 +1,21 @@
 // imports
 import Link from "next/link";
+import styles from "./NotFound.module.scss";
 
 export default function Notfound() {
     return(
-        <div>
-            <h2> Page not found</h2>
-            <p>could not find the page you where looking for</p>
-            <Link href={"/"}> 
-                Go back home
-            </Link>
+        <div className={styles.error}>
+            <div>
+                <h2> Page not found</h2>
+                <p>could not find the page you where looking for</p>
+                <p>
+
+                <Link href={"/"} className={styles.yellowBtn}> 
+                    Go back home
+                </Link>
+                </p>
+            </div>
+            
         </div>
         
     )
