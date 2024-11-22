025148a9e9015efa6a024de64307d03970ba187d
M-Ragini <112775673+M-Ragini@users.noreply.github.com>
2024-11-21 23:35:28 -0700

    added links to privacy policy and cookie policy

--- airitreats/src/components/Footer/Footer.tsx
+++ airitreats/src/components/Footer/Footer.tsx
@@ -10,9 +10,9 @@ export default function Footer() {
         <footer className={styles.footer}>
             <div className={styles.container}>
 
-            <div className={styles.address}>
-                <Map />
-                {/* <div>
+                <div className={styles.address}>
+                    <Map />
+                    {/* <div>
                     <h3> Come Find Us!</h3>
                     <div>
                         <FaMapMarkerAlt title="our store"/>
@@ -24,27 +24,30 @@ export default function Footer() {
                         <p>(XXX) XXX-XXXX</p>
                     </div>
                 </div> */}
-            </div>
-
-            <div className={styles.community}>
-                <div className={styles.socialMedia}>
-                    <h3>Follow Us</h3>
-                    <p><a href="https://www.instagram.com/airitreats/" target="_blank"><FaInstagram title="our instagram"/></a></p>
-                    <p><a href="https://www.facebook.com/profile.php?id=61553694755284" target="_blank"><FaFacebookF title="our facebook"/></a></p>
-
                 </div>
-                <div className={styles.subscription}>
-                    <h3>Newsletter</h3>
-                    <p>Do you want to know the latest deals, new arrivals, limited edition treats? Subscribe to our newsletter!</p>
-                    <Newsletter />
-                </div>
-            </div>
 
-            <div className={styles.legalese}>
-                <a href="">Privacy Policy</a>
-                <span>|</span>
-                <a href="">Terms and Conditions</a>
-            </div>
+                <div className={styles.community}>
+                    <div className={styles.socialMedia}>
+                        <h3>Follow Us</h3>
+                        <p><a href="https://www.instagram.com/airitreats/" target="_blank"><FaInstagram title="our instagram" /></a></p>
+                        <p><a href="https://www.facebook.com/profile.php?id=61553694755284" target="_blank"><FaFacebookF title="our facebook" /></a></p>
+
+                    </div>
+                    <div className={styles.subscription}>
+                        <h3>Newsletter</h3>
+                        <p>Do you want to know the latest deals, new arrivals, limited edition treats? Subscribe to our newsletter!</p>
+                        <Newsletter />
+                    </div>
+                </div>
+
+                <div className={styles.legalese}>
+
+                    <a href="https://www.iubenda.com/privacy-policy/70842258">Privacy Policy</a>
+                    <span>|</span>
+                    <a href="https://www.iubenda.com/privacy-policy/70842258/cookie-policy">Cookie Policy</a>
+                    <span>|</span>
+                    {/* <a href="https://www.iubenda.com/terms-and-conditions/70842258">Terms and Conditions</a> */}
+                </div>
             </div>
 
         </footer>
