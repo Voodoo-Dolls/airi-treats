711af803fc68c5473a708182f4a0996bdb84542d
Michael <michaeldam.dev@gmail.com>
2024-11-28 10:53:57 -0700

    Removed 404 routing on modded URL

--- airitreats/src/app/products/[uid]/[productid]/page.tsx
+++ airitreats/src/app/products/[uid]/[productid]/page.tsx
@@ -11,7 +11,7 @@ export default async function Page({ params }: { params: { productid: string, ui
     const { productid, uid } = params
     const client = createClient();
     // Grab Product Details Using URL Slug
-    let productData: any = await client.getByUID("product", productid).catch(() => notFound());
+    let productData: any = await client.getByUID("product", productid);
     let slug = (productData.data.category.uid)
     if (uid != slug) {
         notFound()
